66a8be0f7e093bc064efaffa5dd0f9517403fa04
winslomk <winslomk@rose-hulman.edu>
2019-03-06 11:03:25 -0500

    Done

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -22,8 +22,7 @@ import rosegraphics as rg
 
 def main():
     print(get_hypotenuse(3,4))
-
-
+    pen_party("blue", 30)
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
@@ -48,7 +47,7 @@ def get_hypotenuse(a, b):
     return c
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -70,14 +69,22 @@ def get_hypotenuse(a, b):
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
-
-
+def pen_party(color, thickness):
+    window = rg.TurtleWindow()
+    window.delay(20)
+    default = rg.SimpleTurtle()
+    custom = rg.SimpleTurtle()
+    default.pen = rg.Pen("green", thickness)
+    custom.pen = rg.Pen(color, 5)
+    default.forward(100)
+    custom.backward(100)
+    window.close_on_mouse_click()
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
